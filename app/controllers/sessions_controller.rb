cf85fba186228c875233a756ee8edfc380f2cb5e
umerijaz08 Ijaz <umer.ijaz@kellogg.northwestern.edu>
2024-03-10 14:37:41 +0000

    Complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by({"email" => params["email"] })
+    if user && user.password == BCrypt::Password.new(user.password)
+      session["user_id"] = user.id
+      redirect_to "/"
+    else
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    redirect_to "/login"
   end
 end
   
